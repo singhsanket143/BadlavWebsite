c53bb5f4c7619833884406d55a0ff457a7ed90af
Sanket Singh <singhsanket143@gmail.com>
2019-01-09 09:48:44 +0530

    Added codes

--- js/main.js
+++ js/main.js
@@ -150,13 +150,22 @@
 var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
+var btn1 = document.getElementById("myBtn1");
+var btn2 = document.getElementById("myBtn2");
+var btn3 = document.getElementById("myBtn3");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal 
-btn.onclick = function() {
+btn1.onclick = function() {
+  modal.style.display = "block";
+  
+}
+btn2.onclick = function() {
+  modal.style.display = "block";
+  
+}
+btn3.onclick = function() {
   modal.style.display = "block";
   
 }
